06e073ebd6a3b967f4f48afd7787fbe735755dab
Johann Sebastian Catalla <catallajohann@gmail.com>
2023-11-14 13:18:09 +0800

    Added Iteach and Ilearn buttons

--- src/Components/swap-buttons/Buttons.js
+++ src/Components/swap-buttons/Buttons.js
@@ -1,23 +1,32 @@
+import React, { useState } from 'react';
+import './Buttons.css'
+
+// Import your images
+import defaultIlearn from './Assets/deselected_ilearn.png';
+import selectedIlearn from './Assets/selected_ilearn.png';
+import defaultIteach from './Assets/deselected_iteach.png';
+import selectedIteach from './Assets/selected_iteach.png';
 import image1 from './Assets/one.jpg';
 import image2 from './Assets/two.jpg';
 
+const MyComponent = () => {
+  const [selectedButton, setSelectedButton] = useState(1);
 
-import React, { useState } from 'react';
-
-function Buttons() {
-  const [image, setImage] = useState('');
-
-  const handleClick = (imgUrl) => {
-    setImage(imgUrl);
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
   }
 
   return (
     <div>
-      <button onClick={() => handleClick(image1)}>Button 1</button>
-      <button onClick={() => handleClick(image2)}>Button 2</button>
-      {image && <img src={image} alt="selected" />}
+      <button onClick={() => handleButtonClick(1)}>
+        <img src={selectedButton === 1 ? selectedIlearn : defaultIlearn} alt="Iteach" />
+      </button>
+      <button onClick={() => handleButtonClick(2)}>
+        <img src={selectedButton === 2 ? selectedIteach : defaultIteach} alt="Ilearn" />
+      </button>
+      <img src={selectedButton === 1 ? image1 : image2} alt="Display" />
     </div>
   );
 }
 
-export default Buttons;
+export default MyComponent;
