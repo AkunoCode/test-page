cfff8e335d3b88a04adbe99884104f75ad3aa82b
Johann Sebastian Catalla <catallajohann@gmail.com>
2023-11-14 14:48:54 +0800

    Added buttons to homepage

--- src/Components/swap-buttons/Buttons.js
+++ src/Components/swap-buttons/Buttons.js
@@ -6,8 +6,8 @@ import defaultIlearn from './Assets/deselected_ilearn.png';
 import selectedIlearn from './Assets/selected_ilearn.png';
 import defaultIteach from './Assets/deselected_iteach.png';
 import selectedIteach from './Assets/selected_iteach.png';
-import image1 from './Assets/one.jpg';
-import image2 from './Assets/two.jpg';
+import image1 from './Assets/banners/ilearn_banner.png';
+import image2 from './Assets/banners/iteach_banner.png';
 
 const MyComponent = () => {
   const [selectedButton, setSelectedButton] = useState(1);
@@ -26,7 +26,7 @@ const MyComponent = () => {
           <img src={selectedButton === 2 ? selectedIteach : defaultIteach} alt="Ilearn" />
         </button>
       </div>
-      <img className="display" src={selectedButton === 1 ? image1 : image2} alt="Display" style={{height: '150px', width: 'auto'}}/>
+      <img className="display" src={selectedButton === 1 ? image1 : image2} alt="Display" />
     </div>
   );
 }
