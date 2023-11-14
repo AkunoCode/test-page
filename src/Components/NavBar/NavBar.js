5536fa8fbb0b39c3aa94d54531f14f45677a85ee
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-11-14 15:53:46 +0800

    modified NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import { Outlet, Link } from 'react-router-dom';
 import "./NavBar.css"
+import logoImg from './images/Logo.png'
+import Search from './images/Search.png'
 
 function NavBar() {
 
@@ -8,15 +10,21 @@ function NavBar() {
             <header id='Header-Scrolled'>
                 <div className='Header-Container'>
                     <div id="Logo-Section">
-                        <h1>Turo-Turo</h1>
+                        <img src={logoImg} alt='Turo-Turo Logo' />
+                        <nav>
+                            <ul id='Navigation-Bar-Scrolled'>
+                                <li><Link to="student">Find Student</Link></li>
+                                <li><Link to="tutor">Find Tutor</Link></li>
+                                <li><Link to="About">About Us</Link></li>
+                            </ul>
+                        </nav>
+                        <div>
+                            <img src={Search} alt='Turo-Turo Logo' />
+                            <input type="text" placeholder="Enter text here..." />
+                        </div>
+
                     </div>
-                    <nav>
-                        <ul id='Navigation-Bar-Scrolled'>
-                            <li><Link to="student">Find Student</Link></li>
-                            <li><Link to="tutor">Find Tutor</Link></li>
-                            <li><Link to="About">About Us</Link></li>
-                        </ul>
-                    </nav>
+
                 </div>
             </header >
             <Outlet />
